23b0c1e17fdf8b9989856d09242b51b1973d0437
Chaerin Kim <decentralized0212@gmail.com>
2021-03-20 00:11:48 +0900

    210319 2292 벌집

---
+++ 2292 벌집.py
@@ -0,0 +1,15 @@
+import math
+a = int(input())
+
+i = 0
+k = 1
+
+if a==1:
+    print(1)
+else:
+    while True:
+        i += 1
+        k = k + 6*i
+        if k > a:
+            print(i+1)
+            break
