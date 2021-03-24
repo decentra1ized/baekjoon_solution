116481f23a9efa84c631095a01396fc6e1b62534
Chaerin Kim <decentralized0212@gmail.com>
2021-03-24 17:41:41 +0900

    Update 2751 수 정렬하기 2.py

--- 2751 수 정렬하기 2.py
+++ 2751 수 정렬하기 2.py
@@ -1,5 +1,13 @@
+num = int(input())
+l = []
+for i in range(num):
+    l.append(int(input()))
+for i in sorted(l):
+    print(i)
+'''
 import sys
 n = int(sys.stdin.readline())
 data = [sys.stdin.readline().strip() for i in range(n)]
 for i in sorted(data):
     sys.stdout.write(str(i)+'\n')
+'''
