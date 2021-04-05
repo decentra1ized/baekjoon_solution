8787906ebca7ebf0cc0033bfaffe809c4dea50b7
Chaerin Kim <decentralized0212@gmail.com>
2021-04-06 00:15:15 +0900

    Update 4153 직각삼각형.py

--- 4153 직각삼각형.py
+++ 4153 직각삼각형.py
@@ -2,7 +2,6 @@ while True:
     a = list(map(int, input().split()))
     a.sort()
     if sum(a) == 0:
-        print('wrong')
         break
     if a[0] ** 2 + a[1] ** 2 == a[2] ** 2:
         print('right')
